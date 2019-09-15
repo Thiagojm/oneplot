6470414ec30bb01fb7d1641714df1fa250e117ea
Thiagojm <tjm.plastica@gmail.com>
2019-09-15 16:10:09 -0300

    update

--- guiplot.py
+++ guiplot.py
@@ -13,20 +13,18 @@ window.geometry('640x480') # window size
 
 window.title("Welcome to GUI Plot App") # window title
  
-lbl = Label(window, text="Hello", font=("Arial Bold", 12)) # Text inside window
+lbl = Label(window, text="Clique para abrir o destino do arquivo (algozscore.txt)", font=("Arial Bold", 11)) # Text inside window
  
 lbl.grid(column=0, row=0) # posição do label
 
 def clicked(): # criar função para quando o botão for clicado
      Tk().withdraw()
-     global data_file
+     global data_file #criar variavel global, pode ser usada fora da função
      data_file = askopenfilename()
     
-btn = Button(window, text="Click Me", bg="white", fg="blue", command=clicked) # criar botão/ command=função do botão
+btn = Button(window, text="Abrir arquivo", bg="white", fg="blue", command=clicked) # criar botão/ command=função do botão
  
 btn.grid(column=1, row=0) # posição do botão
-
-btn.focus() #foco
 
 plt.rcParams["figure.figsize"] = (17,7)
 fig = plt.figure()
@@ -48,12 +46,16 @@ def animate(i):
     ax1.set_title(data_file)
     ax1.set_xticks(ax1.get_xticks()[::60])
 
+lbl2 = Label(window, text="Clique para plotar o gráfico", font=("Arial Bold", 11)) # Text inside window
+ 
+lbl2.grid(column=0, row=4) # posição do label
+
 def plot():
     ani = animation.FuncAnimation(fig, animate, interval=1000)
     plt.show()
 
-btn2 = Button(window, text="Click Me", bg="white", fg="blue", command=plot) # criar botão/ command=função do botão
+btn2 = Button(window, text="Plotar", bg="white", fg="blue", command=plot) # criar botão/ command=função do botão
  
-btn2.grid(column=2, row=0) # posição do botão
+btn2.grid(column=1, row=4) # posição do botão
  
 window.mainloop() # need loop to maintain it open
